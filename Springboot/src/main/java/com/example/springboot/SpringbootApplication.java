30de15680de665226e45c6f49610279ca4c99a88
Ocean456 <95454779+Ocean456@users.noreply.github.com>
2023-06-11 11:53:40 +0800

    Update SpringbootApplication.java

--- Springboot/src/main/java/com/example/springboot/SpringbootApplication.java
+++ Springboot/src/main/java/com/example/springboot/SpringbootApplication.java
@@ -12,7 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.example.springboot.mapper")
 public class SpringbootApplication {// Spring Boot应用程序的主类
-    // 下头男应用程序的入口函数
+    // Spring Boot应用程序的入口函数
     public static void main(String[] args) {
         // 启动Spring Boot应用程序
         SpringApplication.run(SpringbootApplication.class, args);
